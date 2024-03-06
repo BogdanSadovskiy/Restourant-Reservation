d6fa9d401550a959f6dc0dbaccb99dab15d846e8
Iryna Pec <138215475+Dmytro4563@users.noreply.github.com>
2024-03-06 20:50:21 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restourant_Reservation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Restourant_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReservePlace_Click(object sender, RoutedEventArgs e)
+        {
+            this.ReservePlace.Visibility = Visibility.Hidden;
+            this.calendar.Visibility = Visibility.Visible;
+            calendar.SelectedDatesChanged += Calendar_SelectedDatesChanged;
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Places.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restourant_Reservation
+{
+    public class Places
+    {
+        int totalPlaces = 15;
+
+        public int getTotalPlaces()
+        {
+            return totalPlaces;
+        }
+    }
+}
